a24b1dba4e1d4f3d498dd841339015f283fdc2a5
Daniel Ale <danielale9291@gmail.com>
2022-09-14 06:59:01 +0100

    create projects

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import * as NextImage from 'next/image';
+
+const originalNextImage = NextImage.default
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
